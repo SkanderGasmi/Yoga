2d4ed25f24b7f62daa1e966f1e5e2d6921513ed0
Skander Gasmi <skandergasmi7@gmail.com>
2023-04-17 23:39:14 +0100

    Added initial yoga.py script

--- yoga.py
+++ yoga.py
@@ -1,8 +1,10 @@
 import os
 import pandas as pd
+import numpy as np
 
 # Get the absolute path of the directory containing the Python script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Define the path to the folder containing the pose folders
 data_dir = os.path.join(script_dir, 'YOGA/content/cleaned/DATASET/TRAIN')
+
